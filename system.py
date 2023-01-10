e15528b6c9dd98ced54472628f0f7cf21a3aab44
Songtuan Lin <songtuan.lin@anu.edu.au>
2023-01-10 14:21:05 +1100

    remove some comments

--- system.py
+++ system.py
@@ -132,8 +132,8 @@ class System:
             state.add(eff)  
         
     def _get_substitutions(self) -> None:
-        """Computing the variable substitution functions for each action
-        in the plan.
+        """Computing the variable substitution functions for each action in the plan.
+        
         """
         self.substitutions = []
         for a in self.plan:
@@ -309,8 +309,6 @@ class System:
             if not atom.negated:
                 conf_add_atoms = self._matching_add_effs(i, atom)
             else:
-                # pos_atoms = [eff.literal for eff in self.substitutions[i][0].effects if not eff.literal.negated]
-                # conf_add_atoms = [a.negate() for a in self._matching_add_effs(i, atom.negate()) if a not in pos_atoms]
                 conf_add_atoms = [a.negate() for a in self._matching_add_effs(i, atom.negate())]
             has_neg_conf = False
             for a in conf_add_atoms:
